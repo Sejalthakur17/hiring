bb96be521077f12b8f9e22767ed312a161225202
Sejal Thakur <sejalthakurcs@gmail.com>
2026-05-19 20:13:50 +0530

    Added Scripts Infrestructure

---
+++ may-2026/devops/alchemyst-devops/scripts/install_worker.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "Updating packages..."
+sudo apt update -y
+
+echo "Installing Python and pip..."
+sudo apt install python3-pip -y
+
+echo "Installing Docker..."
+sudo apt install docker.io -y
+
+echo "Starting Docker..."
+sudo systemctl start docker
+sudo systemctl enable docker
+
+echo "Worker setup completed."
