5b669da65287804ec35edde04015f13d4e1fc99d
Sejal Thakur <sejalthakurcs@gmail.com>
2026-05-19 20:02:00 +0530

    Added Api Infrestructure

---
+++ may-2026/devops/alchemyst-devops/api/main.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI
+import requests
+
+app = FastAPI()
+
+workers = [
+    "http://10.0.2.10:9000/process",
+    "http://10.0.2.11:9000/process",
+    "http://10.0.2.12:9000/process"
+]
+
+counter = 0
+
+@app.get("/")
+def health():
+    return {
+        "status": "api running"
+    }
+
+@app.post("/infer")
+def infer():
+
+    global counter
+
+    worker = workers[counter % len(workers)]
+
+    counter += 1
+
+    response = requests.post(worker)
+
+    return response.json()
