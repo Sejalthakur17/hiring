3b95788d4edf4857293cf1821d76075536a99e22
Sejal Thakur <sejalthakurcs@gmail.com>
2026-05-21 22:35:37 +0530

    Updated Scripts

--- may-2026/devops/alchemyst-devops/scripts/install_api.sh
+++ may-2026/devops/alchemyst-devops/scripts/install_api.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-echo "Updating packages..."
+echo "Updating packages.."
 sudo apt update -y
 
-echo "Installing Python and pip..."
+echo "Installing Python and pip.."
 sudo apt install python3-pip -y
 
-echo "Installing Docker..."
+echo "Installing Docker.."
 sudo apt install docker.io -y
 
-echo "Starting Docker..."
+echo "Starting Docker.."
 sudo systemctl start docker
 sudo systemctl enable docker
 
